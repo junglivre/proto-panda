5f3aa43a2605acdf3c72be88db8f3fe547ec6c16
Mock <matheus.mtb7@gmail.com>
2026-08-29 18:52:46 -0300

    3.3.3a

--- include/tools/config_default.hpp
+++ include/tools/config_default.hpp
@@ -2,7 +2,7 @@
 /*
     Avoid changing this vile, change the config.hpp instead if you need custom configuration
 */
-#define PANDA_VERSION "3.2.3"
+#define PANDA_VERSION "3.3.3a"
 /*
 Cache file version to invalidate cache in case of firmware update
 */
