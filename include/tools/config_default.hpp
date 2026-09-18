4b5faa4dbd1cf89b110e9949103542b83a39cf90
jung <eu@jung.moe>
2026-09-18 12:34:37 -0300

    fix: revert old version

--- include/tools/config_default.hpp
+++ include/tools/config_default.hpp
@@ -2,7 +2,7 @@
 /*
     Avoid changing this vile, change the config.hpp instead if you need custom configuration
 */
-#define PANDA_VERSION "3.3.6-rc.1"
+#define PANDA_VERSION "3.3.5a"
 /*
 Cache file version to invalidate cache in case of firmware update
 */
